a80d40a454d335f740be81b67257b0dceeb71975
yuura <yuura.exe@gmail.com>
2026-09-06 15:13:12 +0900

    ✨ Enable clipboard sharing across terminal tools

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,22 @@ require("config.lazy")
 vim.opt.termguicolors = true
 vim.opt.mouse = ""
 
+-- OSC52経由でヤンクをローカルのOSクリップボードに同期する（SSH越しでも共有できる）
+-- ペーストはOSC52クエリがtmux越しでは応答されないことが多いため、
+-- 端末側のネイティブペースト（bracketed paste）に任せる
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
+vim.opt.clipboard = "unnamedplus"
+
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
